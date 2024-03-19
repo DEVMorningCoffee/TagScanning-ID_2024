e5e3a7e3a60bf83100f635b55a17fa28bb8d459d
Jeffrey Abraham <Jeffreyabraham.sep+github.com>
2024-03-19 10:57:22 -0700

    Initial Commit

---
+++ TagScanning-ID_2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TagScanning-ID_2024
+//
+//  Created by user238598 on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TagScanning-ID_2024/TagScanning_ID_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  TagScanning_ID_2024App.swift
+//  TagScanning-ID_2024
+//
+//  Created by user238598 on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TagScanning_ID_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
